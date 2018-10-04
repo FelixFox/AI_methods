e05188345c3f184fabe68d6bb0173ab508f2e8eb
Anastasia <a.kryuchkovska@cvrndteam.com>
2018-10-04 19:20:29 +0300

    [Add] 1 lab, initial commit

---
+++ lab1/graph.py
@@ -0,0 +1,163 @@
+
+
+class Graph(object):
+    def __init__(self, graph_dict=None):
+        if graph_dict == None:
+            graph_dict = {}
+        self.__graph_dict = graph_dict
+
+    def vertices(self):
+        return list(self.__graph_dict.keys())
+
+    def edges(self):
+        return self.__generate_edges()
+
+    def add_vertex(self, vertex):
+        if vertex not in self.__graph_dict:
+            self.__graph_dict[vertex] = []
+
+    def add_edge(self, edge):
+        edge = set(edge)
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
+
+    def __generate_edges(self):
+        edges = []
+        for vertex in self.__graph_dict:
+            for neighbour in self.__graph_dict[vertex]:
+                if [neighbour, vertex] not in edges:
+                    edges.append([vertex, neighbour])
+        return edges
+
+    def __str__(self):
+        res = 'vertices: '
+        for k in self.__graph_dict:
+            res += str(k)+' '
+        res += '\nedges: '
+        for edge in self.__generate_edges():
+            res += str(edge) + ' '
+        return res
+
+    def is_connected(self,
+                     vertices_encountered=None,
+                     start_vertex=None):
+        """ determines if the graph is connected """
+        if vertices_encountered is None:
+            vertices_encountered = set()
+        gdict = self.__graph_dict
+        vertices = list(gdict.keys())  # "list" necessary in Python 3
+        if not start_vertex:
+            # chosse a vertex from graph as a starting point
+            start_vertex = vertices[0]
+        vertices_encountered.add(start_vertex)
+        if len(vertices_encountered) != len(vertices):
+            for vertex in gdict[start_vertex]:
+                if vertex not in vertices_encountered:
+                    if self.is_connected(vertices_encountered, vertex):
+                        return True
+        else:
+            return True
+        return False
+
+
+    def connected_components(self):
+    
+        # List of connected components found. The order is random.
+        result = []
+
+        
+    
+        # Make a copy of the set, so we can modify it.
+        nodes = self.vertices()
+        nodes = set(nodes)
+    
+        # Iterate while we still have nodes to process.
+        while nodes:
+    
+            # Get a random node and remove it from the global set.
+            n = nodes.pop()
+    
+            # This set will contain the next group of nodes connected to each other.
+            group = {n}
+    
+            # Build a queue with this node in it.
+            queue = [n]
+    
+            # Iterate the queue.
+            # When it's empty, we finished visiting a group of connected nodes.
+            while queue:
+    
+                # Consume the next item from the queue.
+                n = queue.pop(0)
+    
+                # Fetch the neighbors.
+                neighbors = set(self.__graph_dict[n])
+    
+                # Remove the neighbors we already visited.
+                neighbors.difference_update(group)
+    
+                # Remove the remaining nodes from the global set.
+                nodes.difference_update(neighbors)
+    
+                # Add them to the group of connected nodes.
+                group.update(neighbors)
+    
+                # Add them to the queue, so we visit them in the next iterations.
+                queue.extend(neighbors)
+    
+            # Add the group to the list of groups.
+            result.append(group)
+    
+        # Return the list of groups.
+        return result
+
+
+if __name__ == '__main__':
+    g = {"a": ["d"],
+         "b": ["c"],
+         "c": ["b", "c", "d", "e"],
+         "d": ["a", "c"],
+         "e": ["c"],
+         "f": []
+         }
+
+    graph = Graph(g)
+
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("Edges of graph:")
+    print(graph.edges())
+
+    print("Add vertex:")
+    graph.add_vertex("z")
+
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("Add an edge:")
+    graph.add_edge({"a", "z"})
+
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("Edges of graph:")
+    print(graph.edges())
+
+    print('Adding an edge {"x","y"} with new vertices:')
+    graph.add_edge({"x", "y"})
+    print("Vertices of graph:")
+    print(graph.vertices())
+    print("Edges of graph:")
+    print(graph.edges())
+
+    print('----------------------------------')
+    number = 0
+    for components in graph.connected_components():
+        names = sorted(node.name for node in components)
+        names = ", ".join(names)
+        print ("Group {}: {}".format(number, names))
+        number += 1
---
+++ lab1/visualization.py
@@ -0,0 +1,34 @@
+import networkx as nx
+from graph import Graph
+import random
+import matplotlib.pyplot as plt
+
+
+def visualize_graph(graph: Graph):
+    G = nx.Graph()
+    edge_list = graph.edges()
+    G.add_edges_from(edge_list)
+    node_list = graph.vertices()
+    n_nodes = len(node_list)
+    pos = {node: (random.randint(0, 50), random.randint(0, 100))
+           for i, node in enumerate(node_list)}
+    print(pos)
+    plt.figure(num=None, figsize=(80, 60), dpi=80, facecolor='w', edgecolor='k')
+    nx.draw_networkx_nodes(G, pos, edge_labels=True, node_size=2000)
+    nx.draw_networkx_edges(G, pos, alpha=0.5, width=6)
+    labels = nx.draw_networkx_labels(G, pos)
+    
+    plt.show()
+
+
+if __name__ == "__main__":
+    g = {"a": ["d"],
+         "b": ["c"],
+         "c": ["b", "c", "d", "e"],
+         "d": ["a", "c"],
+         "e": ["c"],
+         "f": []
+         }
+
+    graph = Graph(g)
+    visualize_graph(graph)
